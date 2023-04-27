7360a51d3137a65f716b85440fd565db2550b388
Alejandro Lozano <mario.lozano@edisa.com>
2023-04-27 15:40:48 +0200

    feat: readme

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,4 +1,4 @@
 import path = require('path')
 
-export const EXTENSION_MENU_PATH = 'styles-guideline-manager'
+export const EXTENSION_MENU_PATH = 'files-templates-manager'
 export const CONFIGS_FOLDER = path.join(__dirname, '../../Configs')
